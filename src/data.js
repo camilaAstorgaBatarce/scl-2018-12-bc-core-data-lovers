d5f410111afebc3431e5b4c05a567dc44302e259
“CatalinaCampos” <catalina.camposcaceres9@gmail.com>
2018-12-20 12:46:45 -0300

    Se imprimer los 151 nombres de pokemones en la consola

--- src/data.js
+++ src/data.js
@@ -1,8 +1,21 @@
+//aquí van las funciones
+
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
 
-window.example = example;
+// const example = () => {
+//   return 'example';
+// };
+
+// window.example = example;
+
+// console.log(POKEMON.pokemon[0]); muestra lo que contiene la posición 0 del array
+
+let resultado = [];
+for (let i = 0; i < POKEMON.pokemon.length; i++){
+
+  resultado.push(POKEMON.pokemon[i].name);
+} 
+console.log(resultado);
+
