80ede569a44285635dbe5587a079ef4cb8174af0
“CatalinaCampos” <catalina.camposcaceres9@gmail.com>
2019-01-03 11:18:39 -0300

    se imprimen pokemones en página principal mas no aparece con el estilo card

--- src/data.js
+++ src/data.js
@@ -1,34 +1,28 @@
 //aquí van las funciones
 
-// window.example = example;
-
-// console.log(POKEMON.pokemon[0]); muestra lo que contiene la posición 0 del array
-/* let result = [];
-function pokemonNames () {
-  for (let i = 0; i < POKEMON.pokemon.length; i++){
-    result.push(POKEMON.pokemon[i].name);
+function dataFilter(data, condition) {
+    let filtered = [];
+    for (let i = 0; i < data.length; i++){
+        for (let e = 0; e < data[i].type.length; e++) {
+            if(data[i].type[e] === condition){
+                filtered.push(data[i]);
+            }
+        }
+    }
+    return filtered;
   }
-  return (result)
-}
 
-function pokemonImages () {
-  for (let i = 0; i < POKEMON.pokemon.length; i++){
-    result.push(POKEMON.pokemon[i].images);
-  }
-  return (result)
-} */
+function pokemon(){
+    let result = [];
+    for (let i = 0; i < data.length; i++){
+        result.push(data[i].name + " " + data[i].num + " " + data[i].type + '<img id="img" src="' + data[i].img+'"/>');
+    } 
+      return (result)
+    }
 
-let data = POKEMON.pokemon;
-function dataFilter (data, condition) {
-  let filtered = [];
-  for (let i = 0; i < data.length; i++){
-      for (let e = 0; e < data[i]["type"].length; e++) {
-          if(data[i].type[e] === condition){
-              filtered.push(data[i]);
-          }
-      }
-  }
-  return filtered;
-}
+    console.log('hola')
+
+
+
 
 
