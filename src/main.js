8de835d1f7c99603d7609f7fb004005314facd02
“CatalinaCampos” <catalina.camposcaceres9@gmail.com>
2019-01-04 12:13:12 -0300

    Separación de lógica y DOM

--- src/main.js
+++ src/main.js
@@ -16,35 +16,6 @@ function globalFilter (condition) {
         }
     })
 }
-//armar caja de pokemones según los boostrap card https://getbootstrap.com/docs/4.2/components/card/
-function pokemonBox (name, number, type, image){
-    let typesFormated = formatTypes (type);
-    return `<div class="col-3">
-    <div class="card w-100 pokemon-box">
-    <div  class="card-img-top">
-    <img class="img-fluid" src="${image}" alt="${name}">
-    </div>
-    <div class="card-body">
-    <p class="card-text">#${number}</p>
-    <p class="card-text">${name}</p>
-    <p class="card-text">${typesFormated}</p>
-    </div>
-    </div>
-    </div>`;
-}
-
-//Se crea una función formaType que separa los tipos por la coma y los convierte en array y esto lo hace el split y esto hace que sean elementos independientes
-function formatTypes (types){
-    let typesHtml = "";
-    for (let i = 0; i < types.length; i++){
-        //span sirve para aplicar estilo a un texto https://developer.mozilla.org/es/docs/Web/HTML/Elemento/span, hace que quede al lado los elementos
-        typesHtml += `<span class="tag-type bg-${types[i].toLowerCase()}">${types[i]}</span>`;
-    }
-    return typesHtml;
-}
-
-let buttonArray = ["Grass", "Poison", "Flying", "Fire", "Water", "Bug", "Normal", "Electric", "Ground", "Fighting", "Psychic", "Rock", "Ice", "Ghost", "Dragon"];
-
 for (let i = 0; i < buttonArray.length; i++){
     globalFilter(buttonArray[i]);
 }
