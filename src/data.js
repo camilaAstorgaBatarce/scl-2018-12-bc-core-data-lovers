8dd865a4f92a47da734fc4375e06896b9af23933
“CatalinaCampos” <catalina.camposcaceres9@gmail.com>
2019-01-09 15:16:32 -0300

    Código funcionando, se deben eliminar errores de bootstrap

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 //aquí van las funciones
-// let data = window.POKEMON.pokemon; // con window. no se visualiza la información
-// let modifiedData = window.POKEMON.pokemon; // esta variable se utilizara de forma global para cuando tengamos pokemones filtrados u ordenados se guarden aqui y se pueda utilizar en todos los sitios (seran los pokemones que se ven en el momento en la pagina). En un comienzo se ven todos por eso le seteamos que sea igual a data.
+// con window. no se visualiza la información
+ // esta variable se utilizara de forma global para cuando tengamos pokemones filtrados u ordenados se guarden aqui y se pueda utilizar en todos los sitios (seran los pokemones que se ven en el momento en la pagina). En un comienzo se ven todos por eso le seteamos que sea igual a data.
 window.pokemons = {
 
 filterData: (data, condition) => {
     let filtered = [];
-    for (let i = 0; i < window.POKEMON.pokemon.length; i++){
-        for (let e = 0; e < window.POKEMON.pokemon[i].type.length; e++) {
-            if(window.POKEMON.pokemon[i].type[e] === condition){
-                filtered.push(window.POKEMON.pokemon[i]);
+    for (let i = 0; i < data.length; i++){
+        for (let e = 0; e < data[i].type.length; e++) {
+            if(data[i].type[e] === condition){
+                filtered.push(data[i]);
             }
         }
     }
@@ -33,7 +33,7 @@ sortData: (dataPokemon, sortBy, sortOrder) => {
           return 0;
         }
       });
-    } else if (sortOrder == false){ // ordena de forma descendente
+    } else if (sortOrder == false){
 		//aplicamos slice para que no modifique el array original y asi tenerlos sin orden por si quiere sacar el orden. El sort hace el trabajo de filtrar ascendente o descendente
       return dataPokemon.slice().sort(function(a, b){
         let x = a[sortBy].toLowerCase();
@@ -48,5 +48,4 @@ sortData: (dataPokemon, sortBy, sortOrder) => {
       });
     }
   }
-}
-}
+}}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 // addEventListener click para todos los botones de tipos de pokemon
-let modifiedData = window.POKEMON.pokemon;
+let data = window.POKEMON.pokemon; // con window. no se visualiza la información
+let modifiedData = data;
 function globalFilter (condition) {
     document.getElementById(condition).addEventListener("click", function(){
         document.getElementById("root").innerHTML = "";
-        let filter = window.pokemons.filterData(window.POKEMON.pokemon, condition);
+        let filter = window.pokemons.filterData(data, condition);
 		// guardamos los pokemones filtrados por tipo actuales en modifiedData
 		modifiedData = filter;
         for (let i = 0; i < filter.length; i++){
@@ -14,28 +15,14 @@ function globalFilter (condition) {
 }
 function pokemon(){
     document.getElementById("root").innerHTML = "";
-    for (let i = 0; i < window.POKEMON.pokemon.length; i++){
-        document.getElementById("root").innerHTML += pokemonBox(window.POKEMON.pokemon[i].name, window.POKEMON.pokemon[i].num, window.POKEMON.pokemon[i].type, window.POKEMON.pokemon[i].img);
+    for (let i = 0; i < data.length; i++){
+        document.getElementById("root").innerHTML += pokemonBox(data[i].name, data[i].num, data[i].type, data[i].img);
     }
 }
 
 //hacemos el llenado de todos los pokemones sin filtrar y sin ordenar con pokemon() y se l edice que en el click en el tipo TODOS tambien lo haga.
 document.getElementById("everyone").onclick = pokemon;
 pokemon();
-
-let selectSort = document.getElementById("select");
-// listener change se ejecuta cada vez que se cambia la opcion del select, con esto cada vez que se cambie aplicamos el orden
-selectSort.addEventListener("change", function(){
-	let valueSelect = selectSort.value;
-	let pokemonsOrdered = modifiedData;
-	if(valueSelect === 'aZOrder'){
-		pokemonsOrdered = window.pokemons.sortData(modifiedData, 'name', true); // currentPokemon es el array de pokemones actuales que se ven, 'name' corresponde a que propiedad del objeto queremos ordener, true corresponde a si es ascendente(true) o descendente(false)
-	}else if(valueSelect === 'zAOrder'){
-		pokemonsOrdered = window.pokemons.sortData(modifiedData, 'name', false);
-	}
-	//console.log(valueSelect, pokemonsOrdered, modifiedData);
-	fillWithPokemons(pokemonsOrdered);
-});
 
 //armar caja de pokemones según los boostrap card https://getbootstrap.com/docs/4.2/components/card/
 function pokemonBox (name, number, type, image){
@@ -65,15 +52,33 @@ function formatTypes (types){
 }
 
 // simplemente le pasas un array de pokemones y llena con html de las cajas con la funcion pokemonBox
-function fillWithPokemons (pokemons){
+function fillWithPokemons (pokemones){
 	document.getElementById("root").innerHTML = "";
-    for (let i = 0; i < pokemons.length; i++){
-        document.getElementById("root").innerHTML += pokemonBox(pokemons[i].name, pokemons[i].num, pokemons[i].type, pokemons[i].img);
+    for (let i = 0; i < pokemones.length; i++){
+        document.getElementById("root").innerHTML += pokemonBox(pokemones[i].name, pokemones[i].num, pokemones[i].type, pokemones[i].img);
     }
 }
 
+//document.getElementById("select").onclick = pokemonWeightIMC;
+
+let selectSort = document.getElementById("select");
+// listener change se ejecuta cada vez que se cambia la opcion del select, con esto cada vez que se cambie aplicamos el orden
+selectSort.addEventListener("change", function(){
+	let valueSelect = selectSort.value;
+	let orderedPokemons = modifiedData;
+	if(valueSelect === 'aZOrder'){
+		orderedPokemons = window.pokemons.sortData(modifiedData, 'name', true); // currentPokemon es el array de pokemones actuales que se ven, 'name' corresponde a que propiedad del objeto queremos ordener, true corresponde a si es ascendente(true) o descendente(false)
+	}else if(valueSelect === 'zAOrder'){
+		orderedPokemons = window.pokemons.sortData(modifiedData, 'name', false);
+	}
+	//console.log(valueSelect, orderedPokemons, modifiedData);
+	fillWithPokemons(orderedPokemons);
+});
+
+
 // globalfilter para taodos los botones de tipos
 let buttonArray = ["Grass", "Poison", "Flying", "Fire", "Water", "Bug", "Normal", "Electric", "Ground", "Fighting", "Psychic", "Rock", "Ice", "Ghost", "Dragon"];
+
 for (let i = 0; i < buttonArray.length; i++){
     globalFilter(buttonArray[i]);
 }
