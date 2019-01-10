ef59b18df80761d8e75f3a7ba287b4f176142466
camilaAstorgaBatarce <camilaastorga03@gmail.com>
2019-01-09 22:07:37 -0300

    Se ordena y deja mas legible las funciones/procesos

--- src/data.js
+++ src/data.js
@@ -1,54 +1,17 @@
-//aquí van las funciones
+//aquí van las funciones de filtrados
 
-let data = POKEMON.pokemon; // con window. no se visualiza la información
-let modifiedData = data; // esta variable se utilizara de forma global para cuando tengamos pokemones filtrados u ordenados se guarden aqui y se pueda utilizar en todos los sitios (seran los pokemones que se ven en el momento en la pagina). En un comienzo se ven todos por eso le seteamos que sea igual a data.
-
-function filterData(data, condition) {
+// Esta funcion filtra todos los pokemones por el tipo
+function filterByType(allPokemon, condition) {
     let filtered = [];
-    for (let i = 0; i < data.length; i++){
-        for (let e = 0; e < data[i].type.length; e++) {
-            if(data[i].type[e] === condition){
-                filtered.push(data[i]);
+    for (let i = 0; i < allPokemon.length; i++){
+        for (let e = 0; e < allPokemon[i].type.length; e++) {
+            if(allPokemon[i].type[e] === condition){
+                filtered.push(allPokemon[i]);
             }
         }
     }
     return filtered;
   }
-
-//armar caja de pokemones según los boostrap card https://getbootstrap.com/docs/4.2/components/card/
-function pokemonBox (name, number, type, image){
-    let typesFormated = formatTypes (type);
-    return `<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
-    <div class="card w-100 pokemon-box">
-    <div  class="card-img-top">
-    <img class="img-fluid" src="${image}" alt="${name}">
-    </div>
-    <div class="card-body">
-    <p class="card-text">#${number}</p>
-    <p class="card-text">${name}</p>
-    <p class="card-text">${typesFormated}</p>
-    </div>
-    </div>
-    </div>`;
-}
-
-//Se crea una función formaType que separa los tipos por la coma y los convierte en array y esto lo hace el split y esto hace que sean elementos independientes
-function formatTypes (types){
-    let typesHtml = "";
-    for (let i = 0; i < types.length; i++){
-        //span sirve para aplicar estilo a un texto https://developer.mozilla.org/es/docs/Web/HTML/Elemento/span, hace que quede al lado los elementos
-        typesHtml += `<div class="tag-type bg-${types[i].toLowerCase()}">${types[i]}</div>`;
-    }
-    return typesHtml;
-}
-
-// simplemente le pasas un array de pokemones y llena con html de las cajas con la funcion pokemonBox
-function fillWithPokemons (pokemons){
-	document.getElementById("root").innerHTML = "";
-    for (let i = 0; i < pokemons.length; i++){
-        document.getElementById("root").innerHTML += pokemonBox(pokemons[i].name, pokemons[i].num, pokemons[i].type, pokemons[i].img);
-    }
-}
 
 //sirve para ordenar. dataPokemon = los pokemones que quieres ordenar, sortBy puede ser 'name' actualmente, sortOrder puede ser true o false donde true es ascendente y false es descendente
 function sortData (dataPokemon, sortBy, sortOrder){
@@ -84,4 +47,3 @@ function sortData (dataPokemon, sortBy, sortOrder){
     }
   }
 }
-
